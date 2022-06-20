0c97c2956243c7a3a36abe646e9f2b56985e6849
izerocrazy <izerocrazy@gmail.com>
2022-06-20 16:03:10 +0800

    * Add Base UI Code

---
+++ Unity/Maker/Assets/Scripts/UI/GameScene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine.UI;
+
+public class GameScene: BaseScene
+{
+    public GameScene()
+    {
+    }
+
+	public override void OnMainRunning()
+	{
+		Log.Info("GameScene OnMainRunning");
+
+		LuaMachine m = Main.Instance.GetModule("LuaMachine") as LuaMachine;
+
+		m.DoFile("LuaFile/Game/main");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		base.Update();
+	}
+}
