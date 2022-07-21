1656fc4619bdcd4f205e32ddd3c9175fef289d8a
izerocrazy <izerocrazy@gmail.com>
2022-07-21 20:53:25 +0800

    + Add Code For Editor & UIEditor

---
+++ Unity/Maker/Assets/Scripts/UI/EditorScene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine.UI;
+
+public class EditorScene: BaseScene
+{
+    public EditorScene()
+    {
+    }
+
+	public override void OnMainRunning()
+	{
+		Log.Info("GameScene OnMainRunning");
+
+		LuaMachine m = Main.Instance.GetModule("LuaMachine") as LuaMachine;
+
+		m.DoFile("LuaFile/Editor/main");
+		m.DoString("MainInit()");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		base.Update();
+	}
+
+    private void OnDestroy()
+    {
+		LuaMachine m = Main.Instance.GetModule("LuaMachine") as LuaMachine;
+		m.DoString("MainUninit()");
+    }
+}
